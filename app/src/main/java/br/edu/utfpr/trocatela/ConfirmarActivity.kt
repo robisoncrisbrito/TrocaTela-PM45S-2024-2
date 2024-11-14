8e839af7ba29a552bc8c5492e88792f0727c0adb
robisoncrisbrito <robison.brito@gmail.com>
2024-11-13 22:09:21 -0300

    .

--- app/src/main/java/br/edu/utfpr/trocatela/ConfirmarActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela/ConfirmarActivity.kt
@@ -2,22 +2,35 @@ package br.edu.utfpr.trocatela
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ConfirmarActivity : AppCompatActivity() {
+
+    private lateinit var tvCod : TextView
+    private lateinit var tvQtd : TextView
+    private lateinit var tvValor : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_confirmar)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        tvCod = findViewById( R.id.tvCod )
+        tvQtd = findViewById( R.id.tvQtd )
+        tvValor = findViewById( R.id.tvValor )
+
+
+        val cod = intent.getIntExtra( "cod", 0 )
+        val qtd = intent.getDoubleExtra( "qtd", 0.0 )
+        val valor = intent.getDoubleExtra( "valor", 0.0 )
+
+        tvCod.text = cod.toString()
+        tvQtd.text = qtd.toString()
+        tvValor.text = valor.toString()
+
     }
 
-    fun btConfimarOnClick(view: View) {}
+    fun btConfimarOnClick(view: View) {
+
+    }
 }
--- app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
@@ -1,16 +1,32 @@
 package br.edu.utfpr.trocatela
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LancamentoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lancamento)
     }
 
     fun btConfimarOnClick(view: View) {
+
+        val etCod : EditText = findViewById( R.id.etCod )
+        val etQtd : EditText = findViewById( R.id.etQtd )
+        val etValor : EditText = findViewById( R.id.etValor )
+
+        val intent = Intent( this, ConfirmarActivity::class.java )
+
+        intent.putExtra( "cod", etCod.text.toString().toInt() )
+        intent.putExtra( "qtd", etQtd.text.toString().toDouble() )
+        intent.putExtra( "valor", etValor.text.toString().toDouble() )
+
+        startActivity( intent )
+
 
     }
     fun btListarOnClick(view: View) {
