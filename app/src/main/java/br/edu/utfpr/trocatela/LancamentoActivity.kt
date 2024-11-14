54066e835b80af47802b0d2948c50ffd3178745f
robisoncrisbrito <robison.brito@gmail.com>
2024-11-13 21:50:21 -0300

    .

--- app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
@@ -1,11 +1,19 @@
 package br.edu.utfpr.trocatela
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class LancamentoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lancamento)
+    }
+
+    fun btConfimarOnClick(view: View) {
+
+    }
+    fun btListarOnClick(view: View) {
+
     }
 }
